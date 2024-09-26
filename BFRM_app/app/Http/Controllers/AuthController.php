612fea2a714dc7d04b33852cf8a3a08077709ccb
Maysara <maysaracs1001@gmail.com>
2024-09-26 10:09:05 +0300

    test login post method

--- BFRM_app/app/Http/Controllers/AuthController.php
+++ BFRM_app/app/Http/Controllers/AuthController.php
@@ -30,4 +30,24 @@ class AuthController extends Controller
         $response = ['user'=>$user, 'token'=>$token];
         return response()->json($response, 200);
     }
+
+    public function login(Request $req)
+    {
+        // validate inputs
+        $rules = [
+            'email'=>'required',
+            'password'=>'required|string'
+        ];
+        $req->validate($rules);
+        // find user email in the user table 
+        $user = User::where('email', $req->email)->first();
+        // if user email found and password is correnct
+        if($user && Hash::check($req->password, $user->password)){
+            $token = $user->createToken('Personal Access Token')->plainTextToken;
+            $response = ['user'=>$user, 'token'=>$token];
+            return response()->json($response, 200);
+        }
+        $response = ['message'=> 'Incorrect email or password'];
+        return response()->json($response,400);
+    }
 }
